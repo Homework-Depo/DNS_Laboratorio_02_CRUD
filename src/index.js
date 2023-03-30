337ff3f15c2f594046f80f968f9165f51031053f
Javier Aponte <31341776+javier-aponte@users.noreply.github.com>
2023-03-30 17:48:03 -0500

    login and register routes and views + bootstrap

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+app.get('/registrarse', (req, res) => {
+  res.render('register');
+});
+
+app.get('/ingresar', (req, res) => {
+  res.render('login');
 });
 
 app.listen(port, () => {
